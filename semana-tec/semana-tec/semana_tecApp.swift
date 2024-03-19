5d385da23448de0400da69d0db525e2a0a7fa367
Alumno <alumno@Mac-53.local>
2024-03-19 11:20:55 -0600

    Initial commit

---
+++ semana-tec/semana-tec/semana_tecApp.swift
@@ -0,0 +1,17 @@
+//
+//  semana_tecApp.swift
+//  semana-tec
+//
+//  Created by Alumno on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct semana_tecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
