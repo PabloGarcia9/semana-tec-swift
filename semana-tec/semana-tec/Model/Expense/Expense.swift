7ec0b5e164d93f52b86bf374aa136f79056c1bda
Mario González <mariogzzmendez02@hotmail.com>
2024-03-21 11:38:48 -0600

    Expenses de vdd

--- semana-tec/semana-tec/Model/Expense/Expense.swift
+++ semana-tec/semana-tec/Model/Expense/Expense.swift
@@ -1,8 +1,8 @@
-//
-//  Expense.swift
-//  semana-tec
-//
-//  Created by Alumno on 21/03/24.
-//
-
 import Foundation
+
+struct expense: Decodable, Identifiable{
+    var id: Int
+    var idProject: Int
+    var idPerson: Int
+    var value: Double
+}
--- semana-tec/semana-tec/Model/Expense/ExpenseViewModel.swift
+++ semana-tec/semana-tec/Model/Expense/ExpenseViewModel.swift
@@ -1,8 +1,32 @@
-//
-//  ExpenseViewModel.swift
-//  semana-tec
-//
-//  Created by Alumno on 21/03/24.
-//
-
 import Foundation
+
+@Observable
+class ExpenseViewModel{
+    var cost = [expense]()
+    
+    init() {
+        cost = load("ExpenseData.json")
+    }
+    
+    func load<T: Decodable> (_ filename: String) -> T {
+        let data: Data
+        
+        guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+            else{
+                fatalError("Couldn't find \(filename) from main bundle.")
+        }
+        
+        do {
+            data = try Data(contentsOf: file)
+        } catch {
+            fatalError("Couldn't find \(filename) from main bundle:\n\(error)")
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode(T.self, from: data)
+        } catch {
+            fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+        }
+    }
+}
